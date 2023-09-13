5734531ddc95e1edffd9a30171877faa8da9fe13
Anurag Shetty <anuragshetty007@gmail.com>
2023-09-13 13:35:43 +0530

    third commit - initial changes

---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Next App",
+    description: "Discover AI"    
+}
+
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
